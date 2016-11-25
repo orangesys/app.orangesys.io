c2ea7802b2b3d36ad69a7ff55a20dc24de69a4b6
gaishimo <gaithimo@gmail.com>
2016-11-25 16:55:21 +0900

    Adjust color codes

--- src/views/app/theme.js
+++ src/views/app/theme.js
@@ -1,32 +1,20 @@
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {
-  deepOrange300, deepOrange400, deepOrange500, deepOrange700, deepOrange600,
-  blueA100, blue300, blue500, blue600, blue700,
-  lightBlue300,
-  cyan300,
-  grey100, grey300, grey400,
-  lime500,
-  yellowA400,
-  yellow300,
-  red400,
-  brown400,
-  amber500,
-  cyanA400,
-  orange300, orange500, orange800,
+  deepOrange400, deepOrange500, deepOrange600,
+  blue300,
+  grey300,
+  orange500,
   white, darkBlack, fullBlack,
 } from 'material-ui/styles/colors';
 import { fade } from 'material-ui/utils/colorManipulator';
 
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: deepOrange500,
+    primary1Color: '#F4511E',
     primary2Color: deepOrange600,
     primary3Color: grey300,
     accent1Color: blue300,
-    accent2Color: orange500,
+    accent2Color: '#FF8224',
     accent3Color: grey300,
     textColor: darkBlack,
     alternateTextColor: white,
