abd94ef93c3d5827f5c1da5a903b26abeb562367
pampers <pepperoni9@gmail.com>
2019-08-12 09:58:14 +0000

    chore: update Firebase config

--- aurelia_project/environments/stage.ts
+++ aurelia_project/environments/stage.ts
@@ -1,4 +1,13 @@
 export default {
   debug: true,
-  testing: false
+  testing: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBu39KDBY3wI_zJ7-eTkgmdw3jc51AI-RU',
+    authDomain: 'dev-app-orangesys.firebaseapp.com',
+    databaseURL: 'https://dev-app-orangesys.firebaseio.com',
+    projectId: 'dev-app-orangesys',
+    storageBucket: 'dev-app-orangesys.appspot.com',
+    messagingSenderId: '138054495234',
+    appId: '1:138054495234:web:ae5d7450d38d4cb9',
+  },
 };
