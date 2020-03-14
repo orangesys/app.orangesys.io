2cf25e589ddb1abae17985c6425c6a20d406973d
TJ <thaddeusjiang@gmail.com>
2020-03-14 21:57:42 +0900

    feat(all): clean

--- src/app/setup/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import type { State as SetupState } from './reducer'
-
-export { default as Plan } from './plan'
-export { default as Payment } from './payment'
-
-export { default as reducer } from './reducer'
-export type { SetupState }
--- src/core/orangesys-api/config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-
-export default {
-  apiEndPoint: ORANGESYS_API_ENDPOINT,
-  apiDebug: (API_DEBUG_MODE || '') === 'true',
-}
