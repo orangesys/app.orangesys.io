c5bc5bc041c3f0b946d02861d451af2eb638089d
TJ <thaddeusjiang@gmail.com>
2020-04-26 17:08:54 +0900

    test(env): move test data to env

--- cypress/integration/app.orangesys.io/base-info.spec.js
+++ cypress/integration/app.orangesys.io/base-info.spec.js
@@ -2,21 +2,22 @@
 
 describe('base info', function() {
   before(function() {
-    cy.fixture('account').as('account')
     cy.visit('/base-info')
   })
 
   it('full company name and full name, save and notice', function() {
-    // TODO: global notice, not depend page
+    const email = Cypress.env('test_email')
+    const company_name = Cypress.env('test_company_name')
+    const full_name = Cypress.env('test_full_name')
 
-    cy.get('input[name="email"]').should('have.value', this.account.email)
+    cy.get('input[name="email"]').should('have.value', email)
 
     cy.get('input[name="companyName"]')
       .clear()
-      .type(this.account.companyName)
+      .type(company_name)
     cy.get('input[name="fullName"]')
       .clear()
-      .type(this.account.fullName)
+      .type(full_name)
 
     cy.get('button[type="submit"]').click()
   })
--- cypress/integration/app.orangesys.io/settings.spec.js
+++ cypress/integration/app.orangesys.io/settings.spec.js
@@ -2,26 +2,14 @@
 
 describe('settings', function() {
   before(function() {
-    cy.fixture('account').as('account')
     cy.visit('/settings')
   })
-
   it('account setting', function() {
-    // TODO: global notice, not depend page
+    const email = Cypress.env('test_email')
 
-    cy.get('input[name="companyName"]').should('have.value', this.account.companyName)
-    cy.get('input[name="fullName"]').should('have.value', this.account.fullName)
-    cy.get('input[name="email"]').should('have.value', this.account.email)
+    cy.get('input[name="email"]').should('have.value', email)
 
     cy.contains('パスワード再設定').click()
     cy.contains('パスワード再設定用のメールを送信しました。')
-  })
-
-  it('payment setting', function() {
-    cy.contains('支払い方法').click()
-    // FIXME: iframe
-    // cy.get('input[name="cardnumber"]')
-    cy.contains('登録').click()
-    cy.contains('Your card number is incomplete.')
   })
 })
