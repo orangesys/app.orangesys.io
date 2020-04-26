5475c149cc5b0c6f5a090b238f6fa99e36da7134
TJ <thaddeusjiang@gmail.com>
2020-04-26 23:21:17 +0900

    e2e test sign-up

--- cypress/integration/app.orangesys.io/sign-up.spec.js
+++ cypress/integration/app.orangesys.io/sign-up.spec.js
@@ -7,7 +7,7 @@ describe('sign up', function() {
   it('fill information and, sign up', () => {
     cy.get('input[name="companyName"]').type('Orangesys')
     cy.get('input[name="fullName"]').type('TEST')
-    cy.get('input[name="email"]').type(`test${Math.random()}@orangesys.io`)
+    cy.get('input[name="email"]').type(`test${Math.floor(Math.random() * 100)}@orangesys.io`)
     cy.get('input[name="password"]').type('testing')
     cy.get('button[type="submit"]').click()
 
