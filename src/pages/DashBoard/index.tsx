606be4960eebb6879eac77ae6c1cf35910588147
TJ <thaddeusjiang@gmail.com>
2020-05-09 22:38:31 +0900

    feat(pages): remove lazy load

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,23 +1,23 @@
 /** @jsx jsx */
+import { useContext } from 'react'
 import { jsx } from '@emotion/core'
 import { RouteComponentProps, Redirect, Location } from '@reach/router'
 
 import { routes } from 'routes'
 import { ViewerContext } from 'contexts/Viewer'
-import { useContext } from 'react'
 
 export const DashBoard = (props: RouteComponentProps) => {
   const { viewer } = useContext(ViewerContext)
-  const { planSelected, serverSetupCompleted } = viewer || {}
+
   if (!viewer?.db) {
     return <Location>{({ location }) => <Redirect from={location.pathname} to={routes.BaseInfo} noThrow />}</Location>
   }
 
-  if (!planSelected) {
+  if (!viewer?.planSelected) {
     return <Location>{({ location }) => <Redirect from={location.pathname} to={routes.OrderPlan} noThrow />}</Location>
   }
 
-  if (!serverSetupCompleted) {
+  if (!viewer?.serverSetupCompleted) {
     return (
       <Location>{({ location }) => <Redirect from={location.pathname} to={routes.ServerSetup} noThrow />}</Location>
     )
